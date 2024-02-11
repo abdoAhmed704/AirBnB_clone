f1acde9155b48d438882b206f35fb2f79053b670
= <abdoahmedsayed704@gmail.com>
2024-02-11 16:52:10 +0200

    Done...

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,33 +1,55 @@
 #!/usr/bin/python3
 import json
-from ..base_model import BaseModel
+import os
+from models.base_model import BaseModel
 
 
 class FileStorage:
-    def __init__(self) -> None:
-        self.__file_path = "file.json"
-        self.__objects = {}
+    """
+
+    """
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        return self.__objects
+        """
+
+        """
+        return FileStorage.__objects
 
     def new(self, obj):
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        """
+
+        """
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
-        objs = {}
-        for obj in self.__objects.keys():
-            objs[obj] = self.__objects[obj].to_dict()
+        """
 
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(objs, f)
+        """
+        obj_dictionary = {}
+        for obj in FileStorage.__objects.keys():
+            obj_dictionary[obj] = FileStorage.__objects[obj].to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(obj_dictionary, f)
+
 
     def reload(self):
-        import os
-        if os.path.isfile(self.__file_path):
-            with open(self.__file_path, "r", encoding="utf-8") as f:
+        """
+
+        """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
                 try:
-                    objs_json = json.load(f)
-                    self.__objects.update(objs_json)
+                    obj_dictionary = json.load(f)
+
+                    for k, v in obj_dictionary.items():
+                        cls_name, object_id = k.split('.')
+
+                        cls = eval(cls_name)
+
+                        inst = cls(**v)
+
+                        FileStorage.__objects[k] = inst
                 except Exception:
                     pass
