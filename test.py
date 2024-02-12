a0e5021adb80c6c7e13d7211debc68bf814e878d
= <abdoahmedsayed704@gmail.com>
2024-02-12 04:33:05 +0200

    Done..

--- test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
-a = "BaseModel 1234-1234-1234"
-
-print(a.split()[1])
